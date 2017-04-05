dc4f33a07abef356ba0bc78d4fb5d67db4f26b16
CYC <yenchen0416@gmail.com>
2017-04-05 15:26:45 +0800

    add instructions

--- Tensorflow/SimulationLinearModel.py
+++ Tensorflow/SimulationLinearModel.py
@@ -1,4 +1,9 @@
-### A simple neural network learning the Linear function 
+### A simple neural network learning the Linear function
+### ---
+### 生成線性資料，並將資料增加一些雜訊，使它更像真實資料
+### 在XY座標平面上呈現資料的分布狀況
+### 建立簡單的神經網絡
+### ---
 
 import tensorflow as tf
 import numpy as np
