0f74f914ec9eeff1d0054e21fedd76b89ae8e2ef
CYC <yenchen0416@gmail.com>
2017-05-28 21:11:13 +0800

    add feature engineering

--- Kaggle/TitanicDataset.py
+++ Kaggle/TitanicDataset.py
@@ -83,4 +83,141 @@ plt.ylabel("Number of passengers")
 
 plt.show()
 
+# define a print function that asserts whether or not a feature has been processed
+def status(feature):
+    print("Processing", feature, ": ok")
+
+def get_combined_data():
+    # reading train data
+    train = pd.read_csv("train.csv")
+
+    # reading test data
+    test = pd.read_csv("test.csv")
+
+    # extracting and then removing the targets from the training data
+    targets = train.Survived
+    train.drop("Survived", 1, inplace=True)
+
+    # merging train data and test data for future feature engineering
+    combined = train.append(test)
+    combined.reset_index(inplace=True)
+    combined.drop("index", inplace=True, axis=1)
+
+    return combined
+
+combined = get_combined_data()
+
+combined.info()
+
+# Extracting the passenger titles
+def get_titles():
+    global combined
+
+    # we extract the title from each name
+    combined["Title"] = combined["Name"].map(lambda name: name.split(",")[1].split(".")[0].strip())
+
+    # a map of more aggregated titles
+    Title_Dictionary = {
+        "Capt": "Officer",
+        "Col": "Officer",
+        "Major": "Officer",
+        "Jonkheer": "Royalty",
+        "Don": "Royalty",
+        "Sir": "Royalty",
+        "Dr": "Officer",
+        "Rev": "Officer",
+        "the Countess": "Royalty",
+        "Dona": "Royalty",
+        "Mme": "Mrs",
+        "Mlle": "Miss",
+        "Ms": "Mrs",
+        "Mr": "Mr",
+        "Mrs": "Mrs",
+        "Miss": "Miss",
+        "Master": "Master",
+        "Lady": "Royalty"
+
+    }
+
+    # we map each title
+    combined["Title"] = combined.Title.map(Title_Dictionary)
+
+get_titles()
+# print(combined.head())
+
+# Processing the ages
+grouped = combined.groupby(["Sex", "Pclass", "Title"])
+
+def process_age():
+    global combined
+
+    # a function that fills the missing values of the Age variable
+    def fillAges(row):
+        if row["Sex"] == "female" and row["Pclass"] == 1:
+            if row["Title"] == "Miss":
+                return 30
+            elif row["Title"] == "Mrs":
+                return 45
+            elif row["Title"] == "Officer":
+                return 49
+            elif row["Title"] == "Royalty":
+                return 39
+
+        elif row["Sex"] == "female" and row["Pclass"] == 2:
+            if row["Title"] == "Miss":
+                return 20
+            elif row["Title"] == "Mrs":
+                return 30
+
+        elif row["Sex"] == "female" and row["Pclass"] == 3:
+            if row["Title"] == "Miss":
+                return 18
+            elif row["Title"] == "Mrs":
+                return 31
+
+        elif row["Sex"] == "male" and row["Pclass"] == 1:
+            if row["Title"] == "Master":
+                return 6
+            elif row["Title"] == "Mr":
+                return 41.5
+            elif row["Title"] == "Officer":
+                return 52
+            elif row["Title"] == "Royalty":
+                return 40
+
+        elif row["Sex"] == "male" and row["Pclass"] == 2:
+            if row["Title"] == "Master":
+                return 2
+            elif row["Title"] == "Mr":
+                return 30
+            elif row["Title"] == "Officer":
+                return 41.5
+
+        elif row["Sex"] == "male" and row["Pclass"] == 3:
+            if row["Title"] == "Master":
+                return 6
+            elif row["Title"] == "Mr":
+                return 26
+
+    combined.Age = combined.apply(lambda r: fillAges(r) if np.isnan(r["Age"]) else r["Age"], axis=1)
+
+    status("age")
+
+process_age()
+#print(combined.info())
+
+# Processing the names
+def process_names():
+    global combined
+    # we clean the Name variable
+    combined.drop("Name", axis=1, inplace=True)
+
+    # encoding in dummy variable
+    titles_dummies = pd.get_dummies(combined["Title"], prefix="Title")
+    combined = pd.concat([combined, titles_dummies], axis=1)
+
+    # removing the title variable
+    combined.drop("Title", axis=1, inplace=True)
+
+    status("names")
 
